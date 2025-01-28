cfaff24bad2ef36f4270a2974ad93527d4f39dd3
Utkirbek Qutfiddinov <utkirbekqutfiddinov@gmail.com>
2025-01-28 17:22:08 +0500

    uppercase

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function showContent(type) {
     if (type === 'tatu') {
         iframe.src = 'files/diploma.pdf';
     } else if (type === 'ielts') {
-        iframe.src = 'files/ielts.jpg';
+        iframe.src = 'files/IELTS.jpg';
     }else if (type === 'pdp') {
         //https://online.pdp.uz/checkCertificateQr/b0c63d91-a441-481c-8f7b-ba4fe0413be2
         iframe.src = 'files/pdp.png';
