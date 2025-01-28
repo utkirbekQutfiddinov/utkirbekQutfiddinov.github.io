5bd548322f23dc879515ac0211b319dc66e29888
Utkirbek Qutfiddinov <utkirbekqutfiddinov@gmail.com>
2025-01-28 17:18:55 +0500

    PDPdagi ta'lim jarayoni va pdp tomonidan berilgan sertifikat qo'shildi

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function showContent(type) {
     const iframe = document.getElementById('contentFrame');
-    if (type === 'diplom') {
+    if (type === 'tatu') {
         iframe.src = 'files/diploma.pdf';
     } else if (type === 'ielts') {
         iframe.src = 'files/ielts.jpg';
+    }else if (type === 'pdp') {
+        //https://online.pdp.uz/checkCertificateQr/b0c63d91-a441-481c-8f7b-ba4fe0413be2
+        iframe.src = 'files/pdp.png';
     }
+
+
     const modal = document.getElementById('myModal');
     modal.style.display = 'block'; // Show the modal
 }
